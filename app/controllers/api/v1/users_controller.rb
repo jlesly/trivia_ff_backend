723ac0f2841dd7912a293a68d40007f8c2146171
jlesly <leslyj2@gmail.com>
2022-01-16 14:50:13 -0500

    updated users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::UsersController < ApplicationController
+    def index
+        user = User.all
+        render json: UserSerializer.new(users)
+    end 
+
+    def new
+        user = User.new
+    end 
+
+    def create
+        user = User.create(user_params)
+            if user.save
+                render json: user, status: :accepted
+            else
+                render json: { errors: user.errors.full_messages}, status: :unprocessable_entity
+    end 
+
+    def show
+        user = User.find(params[:id])
+    end 
+
+    private
+
+        def user_params
+            params.require(:user).permit(:username, :score)
+        end 
 end
