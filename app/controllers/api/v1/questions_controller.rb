9674001bc3b6ebd6737810a0df12b555fa9b0857
jlesly <leslyj2@gmail.com>
2022-01-16 14:36:36 -0500

    updated questions controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,3 +1,28 @@
 class Api::V1::QuestionsController < ApplicationController
-    
+    def index
+        questions = Question.all
+        render json: QuestionSerializer.new(questions)
+    end 
+
+    def new
+        question = Question.new
+    end 
+
+    def create
+        question = Question.create(question_params)
+        if question.save
+            render json: question, status: :accepted
+        else
+            render json: {errors: question.errors.full_messages}, status: :unprocessable_entity
+    end 
+
+    def show
+        question = Question.find(params[:id])
+    end 
+
+    private
+
+        def question_params
+            params.require(:question).permit(:prompt, :option, :answer, :category_id)
+        end 
 end
