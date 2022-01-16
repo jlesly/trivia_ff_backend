803650592c40cbaa0df3a4557c3a698c2e65f2ee
jlesly <leslyj2@gmail.com>
2022-01-16 02:18:54 -0500

    updated tables

--- db/migrate/20220116071444_create_questions.rb
+++ db/migrate/20220116071444_create_questions.rb
@@ -1,7 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-
+      t.string :prompt
+      t.string :option
+      t.string :answer
+      t.references :category, null: false, foreign_key: true
       t.timestamps
     end
   end
