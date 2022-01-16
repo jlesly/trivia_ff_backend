5b940e2d9187d0b4edbada1f26fea5c09644367a
jlesly <leslyj2@gmail.com>
2022-01-16 14:15:55 -0500

    added methods to categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::CategoriesController < ApplicationController
+    def index
+        @categories = Category.all
+        render json: @categories
+    end 
+
+    def create
+        @category.new(category_params)
+        if @category.save
+            render json: @category, status: :accepted
+        else
+            render json: { errors: @category.errors.full_messages }, status: :unprocessable_entity
+    end 
+
+    private
+        def category_params
+            params.require(:category).permit(:name)
+        end
 end
